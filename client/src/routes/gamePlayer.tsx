133e5b2b44f67ebc65cc63709ef57c165a8a30af
Florian <58337160+OmengU@users.noreply.github.com>
2024-01-18 10:39:14 +0100

    frontend: further work on game.

--- client/src/routes/gamePlayer.tsx
+++ client/src/routes/gamePlayer.tsx
@@ -1,39 +1,70 @@
 import { useLocation } from "react-router-dom";
-import { GameQuestionDto, QType, colors } from "../global";
+import { GameQuestionDto, QType, colors, sURL } from "../global";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 import { useState } from "react";
+import * as signalR from "@microsoft/signalr";
 
 const GamePlayer = () => {
+    let connection = new signalR.HubConnectionBuilder()
+        .withUrl(`${sURL}/Game`)
+        .build();
+
+    connection.start()
+        .then(() => console.log('Connection started!'))
+        .catch(() => console.log('Error while establishing connection :('));
+    
     const location = useLocation();
     const data: GameQuestionDto = location.state;
 
     const [multiIds, setMultiIds] = useState<string[]>([]);
+    const [question, setQuestion] = useState<GameQuestionDto>(data);
+    const [score, setScore] = useState<number>(0);
+    const [answered, setAnswered] = useState<boolean>(false);
 
-    const type: QType = QType.MultipleChoice;
+    connection.on("correctResult", () => {
+        console.log("question is correct");
+    })
+
+    connection.on("submitRecieved", () => {
+        setAnswered(true);
+    })
+
+    const type: QType = QType.SingleChoice;
+
+    const sendToHub = (ids: string[]) => {
+        connection.invoke("UpdateScore", localStorage.getItem('playerId'), ids)
+                            .catch(err => console.error(err));
+    }
 
     const selectMulti = (event: React.MouseEvent<HTMLButtonElement>, id: string) => {
             event.preventDefault();
             multiIds.includes(id) ? setMultiIds([...multiIds.filter((m) => m != id)]) : setMultiIds([...multiIds, id]);
     }
 
-    const submitSelection = (event: React.MouseEvent<HTMLButtonElement>, id: string) => {
+    const submitSingle = (event: React.MouseEvent<HTMLButtonElement>, id: string) => {
             event.preventDefault();
-            //todo
+            sendToHub([id]);
+    }
+
+    const submitMulti = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        sendToHub(multiIds);
     }
 
     return <>
         <Flex direction={{ base: "column", md: "row" }} wrap="wrap" justify={{ base: "flex-start", md: "center" }} h={type == QType.MultipleChoice ? "90vh" : "100vh"} p={"1.5rem"}>
-            {data.options.map((o, i) =>
+            {question.options.map((o, i) =>
                 <Box p="1rem" key={i}
-                    minW={{ base: "100%", md: data.options.length <= 3 ? `${100 / data.options.length}%` : "33.33%" }}
-                    h={{ base: `${100 / data.options.length}%`, md: data.options.length <= 3 ? "100%" : "50%" }}>
+                    minW={{ base: "100%", md: question.options.length <= 3 ? `${100 / question.options.length}%` : "33.33%" }}
+                    h={{ base: `${100 / question.options.length}%`, md: question.options.length <= 3 ? "100%" : "50%" }}>
                     <Button p={"1.5rem"}
                         bg={type == QType.MultipleChoice && !multiIds.includes(o.oId) ? "white" : colors[i]}
                         color={type == QType.MultipleChoice && !multiIds.includes(o.oId) ? "black" : "white"}
                         border={type == QType.MultipleChoice && !multiIds.includes(o.oId) ? `10px solid ${colors[i]}` : "none"}
                         minW={"100%"}
                         h={"100%"}
-                        onClick={type == QType.MultipleChoice ? (event) => selectMulti(event, o.oId) : (event) => submitSelection(event, o.oId)}
+                        isDisabled={answered}
+                        onClick={type == QType.MultipleChoice ? (event) => selectMulti(event, o.oId) : (event) => submitSingle(event, o.oId)}
                         _hover={type == QType.MultipleChoice && !multiIds.includes(o.oId) ? { bg: colors[i], border: `none`, color: "white" } :
                             { bg: "white", border: `4px solid ${colors[i]}`, color: "black" }}>
                         <Text fontSize={"5xl"}>
@@ -45,7 +76,7 @@ const GamePlayer = () => {
         </Flex>
         {type == QType.MultipleChoice ?
             <Box w={"100vw"} p={".7rem"} h={"10vh"}>
-                <Button colorScheme="green" w={"100%"} h={"100%"} size={"lg"} fontSize={"3xl"}>
+                <Button colorScheme="green" w={"100%"} h={"100%"} size={"lg"} fontSize={"3xl"} isDisabled={answered} onClick={(event) => submitMulti(event)}>
                     Submit
                 </Button>
             </Box>
--- client/src/routes/gameTutor.tsx
+++ client/src/routes/gameTutor.tsx
@@ -1,38 +1,59 @@
 import { useLocation } from "react-router-dom";
-import { GameQuestionDto, QType, colors } from "../global";
+import { GameQuestionDto, Player, QType, colors, sURL } from "../global";
 import { Badge, Box, Button, Card, CardBody, Flex, Text } from "@chakra-ui/react";
+import * as signalR  from "@microsoft/signalr";
+import { useState } from "react";
 
 const GameTutor = () => {
+    let connection = new signalR.HubConnectionBuilder()
+        .withUrl(`${sURL}/Game`)
+        .build();
+
+    connection.start()
+        .then(() => console.log('Connection started!'))
+        .catch(() => console.log('Error while establishing connection :('));
+    
     const location = useLocation();
     const data: GameQuestionDto = location.state;
     console.log(data);
+
+    const[question, setQuestion] = useState<GameQuestionDto>(data);
+
+    connection.on("topPlayers", (scoreList: Player[]) => {
+        console.log(scoreList);
+    });
 
     return <>
         <Box>
             <Flex h={"33vh"} bgGradient={"linear(to-l, #7928CA, #FF0080)"} direction={"row"} align={"center"} justify={"center"} p={"1rem"} gap={"2rem"}>
                 <Flex align={"center"} h={"60%"} bg={"white"} borderRadius={"15px"} p={"1.5rem"}>
                     <Text fontSize={"5xl"}>
-                        ⏱️{data.timeLimit}
+                        ⏱️{question.timeLimit}
                     </Text>
                 </Flex>
                 <Flex align={"center"} h={"60%"} bg={"white"} borderRadius={"15px"} p={"1.5rem"} >
                     <Text fontSize={"5xl"}>
-                        {data.questionName}
+                        {question.questionName}
                     </Text>
                 </Flex>
                 <Flex direction={"column"} justify={"center"} align={"center"} h={"60%"} bg={"white"} borderRadius={"15px"} p={"1.5rem"} gap={".5rem"}>
                     <Text fontSize={"4xl"}>
-                        🏆{data.points}
+                        🏆{question.points}
                     </Text>
-                    <Badge colorScheme={data.type == QType.MultipleChoice ? 'pink' : 'purple'} fontSize={"l"}>
-                        {data.type == QType.MultipleChoice ? 'Multi Choice' : 'Single Choice'}
+                    <Badge colorScheme={question.type == QType.MultipleChoice ? 'pink' : 'purple'} fontSize={"l"}>
+                        {question.type == QType.MultipleChoice ? 'Multi Choice' : 'Single Choice'}
                     </Badge>
                 </Flex>
                 <Flex bg={"white"} p={"1.5rem"} borderRadius={"15px"} align={"center"} h={"60%"}>
                     <Button size={"lg"} h={"100%"} fontSize={"xl"} p={"1.25rem"}
                         bgGradient={"linear(to-l, #7928CA, #FF0080)"}
                         color={"white"}
-                        _hover={{ bgGradient: "linear(to-r, #7928CA, #FF0080)" }}>
+                        _hover={{ bgGradient: "linear(to-r, #7928CA, #FF0080)" }}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            connection.invoke("RequestScores")
+                            .catch(err => console.error(err));
+                        }}>
                         Next Question
                     </Button>
                 </Flex>
@@ -40,8 +61,8 @@ const GameTutor = () => {
         </Box>
 
         <Flex wrap="wrap" justifyContent="center" h={"67vh"} p={"2.5rem"} gap={"1.5rem"}>
-            {data.options.map((o, i) =>
-                <Box minW={data.options.length <= 3 ? `${100 / data.options.length}%` : "33.33%"} key={i}>
+            {question.options.map((o, i) =>
+                <Box minW={question.options.length <= 3 ? `${100 / question.options.length}%` : "33.33%"} key={i}>
                     <Card bg={colors[i]} h={"100%"}>
                         <CardBody display="flex" justifyContent="center" alignItems="center">
                             <Text fontSize={"8xl"} color={"white"}>
